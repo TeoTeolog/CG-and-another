b0a890d64c6be774bdac7ef14a90b4cde721cbae
TeoTeolog <petr.faylov@mail.ru>
2022-11-28 18:43:13 +0300

    fourth lab

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import numpy as np
 from myKinter import *
 
 root = Tk()
@@ -10,14 +9,19 @@ class State:
 
     def __init__(self):
         self.pointQueue = ['first', 'second', 'third', 'center']
+        self.dotList = []
         self.existPointList = []
         self.curPointNum = 0
-        self.curPoint = self.pointQueue[self.curPointNum]
+        # self.curPoint = self.pointQueue[self.curPointNum]
+        self.curPoint = None
         self.linesList = []
         self.textCorner = StringVar()
         self.xValue = StringVar()
         self.yValue = StringVar()
-        self.pointList = None
+        self.x1Value = StringVar()
+        self.y1Value = StringVar()
+        self.zValue = StringVar()
+        self.pointList = np.array([])
         self.linePoints = []
         self.lastPoint = []
 
@@ -59,14 +63,11 @@ def firstLab():
                    (pointCords.y1 - centerCords.y) * math.cos(t)
             return Cords([pointCords.x, pointCords.y, newX, newY], 'x1y1')
 
-
     def getRandomColor():
         n = hex(random.randrange(0, 256 * 256 * 256 - 1)).replace('0x', '#')
         if len(n) < 7:
             n = n[0] + '0' * (7 - len(n)) + n[1:]
-        print('color:',n)
         return n
-
 
     state = State()
     c = Canvas(root, width=30 * SPACE + 100, height=500, bg='white')
@@ -75,15 +76,12 @@ def firstLab():
 
     myBtnField = ButtonField(Cords([0, 0, 30 * SPACE + 100, 500]))
     myDC = DrowingCanvas(Cords([15 * SPACE, 0, 15 * SPACE + 100, 500]))
+    myDC.displayGrid()
     myBtnField.addObject(myDC)
     MAINPOINT = ['first', 'second', 'third']
 
-
-
-
     def btnClick(event):
         state.setCurPoint(event.btnPressed.name)
-
 
     def deleteLine(firstPoint, secondPoint):
         try:
@@ -98,10 +96,10 @@ def firstLab():
         finally:
             return 0
 
-
     def addNewLine(firstPoint, secondPoint):
         deleteLine(firstPoint, secondPoint)
-        newLine = Line(Cords([secondPoint.cords.x, secondPoint.cords.y, firstPoint.cords.x, firstPoint.cords.y], 'x1y1'),
+        newLine = Line(Cords([secondPoint.cords.x, secondPoint.cords.y, firstPoint.cords.x, firstPoint.cords.y],
+                             'x1y1'),
                        firstPoint.name + secondPoint.name,
                        'red')
         state.addLine(newLine.name)
@@ -109,7 +107,6 @@ def firstLab():
         secondPoint.addChild(newLine.name)
 
         return newLine
-
 
     def deletePointWithChild(pointName):
         if pointName in myDC.objectArray:
@@ -120,9 +117,7 @@ def firstLab():
             myDC.objectArray.pop(pointName)
             del pointName
 
-
     def newClick(event):
-        n = 0
         try:
             n = int(state.textCorner.get())
         except:
@@ -140,7 +135,6 @@ def firstLab():
                     rotateCords(math.radians(1), myDC.objectArray[i].cords, myDC.objectArray['center'].cords, 'x1y1'), j
                 )
 
-
     def clickHandler(event):
         deletePointWithChild(state.curPoint)
         newPoint = Button(event.cords, state.curPoint, color=getRandomColor())
@@ -153,15 +147,12 @@ def firstLab():
         newPoint.onClick(btnClick)
         myDC.addObject(newPoint)
 
-
     def SpeedUP(event):
         if myRoot.getTimeStep() > 5:
             myRoot.setTimeStep(myRoot.getTimeStep() - 5)
 
-
     def SpeedDown(event):
         myRoot.setTimeStep(myRoot.getTimeStep() + 5)
-
 
     def App():
         myDC.setOnClick(clickHandler)
@@ -203,15 +194,8 @@ def secondLab():
 
     myBtnField = ButtonField(Cords([0, 0, 30 * SPACE + 100, 500]))
     myDC = DrowingCanvas(Cords([0, 200, 30 * SPACE + 100, 300]))
+    myDC.displayGrid()
     myBtnField.addObject(myDC)
-
-    def createDot(x, y, color='red', width=5):
-        return myRoot.canvas.create_oval(x, y, x + width, y + width, width=0,
-                                  fill=color)
-
-    def createPoint(x, y, color='red', width=5):
-        cords = myDC.calculateCordsByXY(x, y)
-        return createDot(cords[0], cords[1], color, width)
 
     def createDotLine(first, second):
         if first:
@@ -229,7 +213,7 @@ def secondLab():
             myRoot.canvas.delete(i)
         for t in step:
             pointCords = calculatePoint(t, points)
-            state.linePoints.append(createPoint(pointCords[0], pointCords[1], width=1, color='#9fc5e8'))
+            state.linePoints.append(myDC.createPoint(pointCords[0], pointCords[1], width=1, color='#9fc5e8'))
 
     def calculatePoint(t, points):
         newPoints = points
@@ -244,38 +228,27 @@ def secondLab():
             newPoints += [point]
         return newPoints
 
+    def addPoint(temp, color):
+        myDC.createPoint(temp[0], temp[1], color=color)
+        if state.pointList.size:
+            state.pointList = np.append(state.pointList, [temp], axis=0)
+        else:
+            state.pointList = np.array([temp])
+        calculate(steps, state.pointList)
+
     state = State()
-    step = np.arange(0, 1, 0.001)
-    state.pointList = np.array([[0, 0], [3, 5], [4, 10]])
-    for i in state.pointList:
-        createPoint(i[0], i[1])
-
-    for i in state.pointList:
-        pointCords = myDC.calculateCordsByXY(i[0], i[1])
-        state.linesList.append(createDotLine(state.lastPoint, pointCords))
-        state.lastPoint = pointCords
-
-    calculate(step, state.pointList)
+    steps = np.arange(0, 1, 0.001)
+    tempPointList = np.array([[0, 0], [3, 5], [4, 10]])
+    for tempPoint in tempPointList:
+        addPoint(tempPoint, 'blue')
 
     def clickHandler(event):
-
-        createDot(event.cords.x, event.cords.y, 'green')
-
-        temp = [myDC.calculateXYbyCords(event.cords.x, event.cords.y)]
-        tempPointList = np.append(state.pointList, temp, axis=0)
-        state.pointList = tempPointList
-        state.xValue.set('')
-        state.yValue.set('')
-        calculate(step, state.pointList)
+        addPoint(myDC.calculateXYbyCords(event.cords.x, event.cords.y), 'green')
 
     def addHandler(event):
-        temp = [float(state.xValue.get()), float(state.yValue.get())]
-        createPoint(temp[0], temp[1])
-        tempPointList = np.append(state.pointList, [temp], axis=0)
-        state.pointList = tempPointList
+        addPoint([int(state.xValue.get()), int(state.yValue.get())], 'red')
         state.xValue.set('')
         state.yValue.set('')
-        calculate(step, state.pointList)
 
     def App():
 
@@ -285,7 +258,7 @@ def secondLab():
         YLable = Lable(Cords([160, 70, 100, 30]), 'input y:')
         EditYCord = MyEdit(cords=Cords([160, 100, 100, 30]), textvariable=state.yValue)
 
-        buttonAdd = Button(Cords([360, 100, 50, 30]), 'downBtn', 'Down')
+        buttonAdd = Button(Cords([300, 100, 50, 30]), 'addButton', 'Add')
         buttonAdd.onClick(addHandler)
         myBtnField.addObject(buttonAdd)
 
@@ -306,5 +279,260 @@ def secondLab():
     c.pack()
 
 
-secondLab()
+def thirdLab():
+
+    c = Canvas(root, width=40 * SPACE, height=800, bg='white')
+    myRoot = Root(root, c)
+    myRoot.setRoot()
+
+    quadrilateral = [] #фигура
+
+    myBtnField = ButtonField(Cords([0, 0, 40 * SPACE, 800]))
+    myDC = DrowingCanvas(Cords([1 * SPACE, 200, 25 * SPACE, 550]))
+    myDC.displayGrid3D()
+    myBtnField.addObject(myDC)
+
+    def createDotLine(first, second):
+        if first:
+            return Line(Cords([first[0], first[1], second[0], second[1]], 'x1y1'),
+                        'line' + str(first[0]) + str(second[0]), 'red')
+        else:
+            return None
+
+    def calculate(step, points):
+        pointCords = myDC.calculateCordsByXY(points[-1][0], points[-1][1])
+        state.linesList.append(createDotLine(state.lastPoint, pointCords))
+        state.lastPoint = pointCords
+
+        for i in state.linePoints:
+            myRoot.canvas.delete(i)
+        for t in step:
+            pointCords = calculatePoint(t, points)
+            state.linePoints.append(myDC.createPoint(pointCords[0], pointCords[1], width=1, color='#9fc5e8'))
+
+    def calculatePoint(t, points):
+        newPoints = points
+        while len(newPoints) > 1:
+            newPoints = Casteljo(t, newPoints)
+        return newPoints[0]
+
+    def Casteljo(t, points):
+        newPoints = []
+        for i in range(0, len(points) - 1):
+            point = (1 - t) * points[i] + t * points[i + 1]
+            newPoints += [point]
+        return newPoints
+
+    def addPoint(temp, color):
+        myDC.create3DPoint(np.array(temp), color=color)
+        if state.pointList.size:
+            state.pointList = np.append(state.pointList, [temp], axis=0)
+        else:
+            state.pointList = np.array([temp])
+        # calculate(steps, state.pointList)
+
+    def editPoint(cords, color):
+        temp = [cords[0], cords[1]]
+        dot = myDC.createPointNew(cords[0], cords[1], pointCords=cords)
+        print('dot', dot)
+        print(dot.pointCords)
+        displayPointList(dot)
+        state.dotList.append(dot)
+
+    def setEditMode(event):
+        print('[setEditMode] event.btnPressed.pointCords', event.btnPressed.pointCords)
+
+    STEP = 30
+
+    # def displayPointList(point):
+    #     print('point {dot}', point)
+    #     print(point.pointCords)
+    #     lable = Lable(Cords([20 * SPACE, 20 + STEP * len(state.existPointList), 100, 10]), str(len(state.existPointList))+str(point.pointCords))
+    #     lable.pointCords = point.pointCords
+    #     lable.connectedPoint = point
+    #     lable.onClick(setEditMode)
+    #     myBtnField.addObject(lable)
+    #     state.existPointList.append(lable)
+
+    state = State()
+    steps = np.arange(0, 1, 0.001)
+    for i in range(16):
+        state.existPointList.append([random.randrange(1, i+2), random.randrange(9, 15), random.randrange(1, i+2)])
+        addPoint(state.existPointList[i], 'red')
+    flag = True
+    jopa = []
+    for i in range(1, 15, 2):
+        if flag:
+            myDC.create3DLine(np.array(state.existPointList[i]), np.array(state.existPointList[i + 1]), 'green')
+            myDC.create3DLine(np.array(state.existPointList[i]), np.array(state.existPointList[i - 1]), 'blue')
+            flag = False
+            jopa = [i+1, i-1]
+        else:
+            myDC.create3DLine(np.array(state.existPointList[i]), np.array(state.existPointList[jopa[0]]), 'green')
+            myDC.create3DLine(np.array(state.existPointList[i]), np.array(state.existPointList[jopa[1]]), 'blue')
+            flag = True
+
+
+    def displayPointList():
+        for i in range(len(state.existPointList)):
+            print('[display]', i, ':', state.existPointList[i])
+            lable = Lable(Cords([30 * SPACE, 70 + STEP * i, 100, STEP - 5]), str(i)+' : '+str(state.existPointList[i]))
+
+    def clickHandler(event):
+        # addPoint(myDC.calculateXYbyCords(event.cords.x, event.cords.y), 'green')
+        displayPointList()
+
+    def addHandler(event):
+        editPoint([float(state.xValue.get()), float(state.yValue.get()), 0], 'red')
+        state.xValue.set('')
+        state.yValue.set('')
+
+    def App():
+
+        NumLable = Lable(Cords([1 * SPACE, 70, 3 * SPACE, 30]), 'number of point:')
+        EditNumber = MyEdit(cords=Cords([1 * SPACE, 100, 3 * SPACE, 30]), textvariable=state.xValue)
+
+        XLable = Lable(Cords([5 * SPACE, 70, 3 * SPACE, 30]), 'input x:')
+        EditXCord = MyEdit(cords=Cords([5 * SPACE, 100, 3 * SPACE, 30]), textvariable=state.xValue)
+
+        YLable = Lable(Cords([9 * SPACE, 70, 3 * SPACE, 30]), 'input y:')
+        EditYCord = MyEdit(cords=Cords([9 * SPACE, 100, 3 * SPACE, 30]), textvariable=state.yValue)
+
+        ZLable = Lable(Cords([13 * SPACE, 70, 3 * SPACE, 30]), 'input z:')
+        EditZCord = MyEdit(cords=Cords([13 * SPACE, 100, 3 * SPACE, 30]), textvariable=state.zValue)
+
+        buttonAdd = Button(Cords([17 * SPACE, 100, 4 * SPACE, 30]), 'addButton', 'Add')
+        buttonAdd.onClick(addHandler)
+        myBtnField.addObject(buttonAdd)
+
+        myDC.setOnClick(clickHandler)
+
+        def key(event):
+            print("pressed", repr(event.char))
+
+        def callback(event):
+            myBtnField.clicked(event)
+
+        c.bind("<Key>", key)
+        c.bind("<Button-1>", callback)
+
+    App()
+
+    c.focus_set()
+    c.pack()
+
+    # i can concatinate dots into figure and figure into dificult stucture
+
+def forthLab():
+    c = Canvas(root, width=30 * SPACE + 100, height=500, bg='white')
+    myRoot = Root(root, c)
+    myRoot.setRoot()
+
+    state = State()
+    # steps = np.arange(0, 1, 0.001)
+
+    myBtnField = ButtonField(Cords([0, 0, 30 * SPACE + 100, 500]))
+    myDC = DrowingCanvas(Cords([0, 240, 30 * SPACE + 100, 260]))
+    myDC.displayGrid()
+    myBtnField.addObject(myDC)
+
+    def createRandomLine(name):
+        return Line(Cords([random.randrange(myDC.cords.x, myDC.cords.x1), random.randrange(myDC.cords.y, myDC.cords.y1),
+                           random.randrange(myDC.cords.x, myDC.cords.x1), random.randrange(myDC.cords.y, myDC.cords.y1)],
+                          'x1y1'),
+                    name, 'red')
+
+    def addLine(temp):
+        tempLine = createRandomLine(str(temp))
+        rect = state.curPoint
+        state.linesList.append(tempLine)
+        k = 4
+        while k:
+            if not(rect.checkCords(tempLine.cords.x0y0) + rect.checkCords(tempLine.cords.x1y1)):
+                print('Ks & Ke тривиально виден')
+                state.linesList.append(Line(Cords([tempLine.cords.x, tempLine.cords.y,
+                                                   tempLine.cords.x1, tempLine.cords.y1], 'x1y1'), 'name', 'blue'))
+                break
+            elif rect.checkCords(tempLine.cords.x0y0) & rect.checkCords(tempLine.cords.x1y1):
+                print('Ks & Ke тривиально не виден')
+                break
+            else:
+                if not(rect.checkCords(tempLine.cords.x0y0)):
+                    tempLine.cords = Cords([tempLine.cords.x1, tempLine.cords.y1, tempLine.cords.x, tempLine.cords.y],
+                                           'x1y1')
+                if rect.checkCords(tempLine.cords.x0y0) & 8:
+                    tempLine.cords.y = formulaY(tempLine, rect.cords[0])
+                    tempLine.cords.x = rect.cords[0]
+                elif rect.checkCords(tempLine.cords.x0y0) & 4:
+                    tempLine.cords.y = formulaY(tempLine, rect.cords[2])
+                    tempLine.cords.x = rect.cords[2]
+                elif rect.checkCords(tempLine.cords.x0y0) & 2:
+                    tempLine.cords.x = formulaX(tempLine, rect.cords[1])
+                    tempLine.cords.y = rect.cords[1]
+                else:
+                    tempLine.cords.x = formulaX(tempLine, rect.cords[3])
+                    tempLine.cords.y = rect.cords[3]
+            tempLine.cords = Cords([tempLine.cords.x, tempLine.cords.y, tempLine.cords.x1, tempLine.cords.y1], 'x1y1')
+            k -= 1
+
+    def formulaY(line, X):
+        t = (X - line.cords.x) / line.cords.width
+        return line.cords.y + t * line.cords.height
+
+    def formulaX(line, Y):
+        t = (Y - line.cords.y) / line.cords.height
+        return line.cords.x + t * line.cords.width
+
+    def addRectangle(temp):
+        sP = myDC.calculateCordsByXY(temp[0], temp[1])
+        eP = myDC.calculateCordsByXY(temp[2], temp[3])
+        state.curPoint = Rectangle([sP[0], sP[1], eP[0], eP[1]])  #сборщик мусора удалит старый объект со всем внутри
+
+    def clickHandler(event):
+        addLine(myDC.calculateXYbyCords(event.cords.x, event.cords.y))
+
+    def addHandler(event):
+        addRectangle([float(state.xValue.get()), float(state.yValue.get()),
+                      float(state.x1Value.get()), float(state.y1Value.get())])
+        state.xValue.set('')
+        state.yValue.set('')
+        state.x1Value.set('')
+        state.y1Value.set('')
+
+    def App():
+
+        XLable = Lable(Cords([30, 70, 100, 30]), 'input x:')
+        EditXCord = MyEdit(cords=Cords([30, 100, 100, 30]), textvariable=state.xValue)
+
+        YLable = Lable(Cords([160, 70, 100, 30]), 'input y:')
+        EditYCord = MyEdit(cords=Cords([160, 100, 100, 30]), textvariable=state.yValue)
+
+        X1Lable = Lable(Cords([30, 130, 100, 30]), 'input x:')
+        EditX1Cord = MyEdit(cords=Cords([30, 170, 100, 30]), textvariable=state.x1Value)
+
+        Y1Lable = Lable(Cords([160, 130, 100, 30]), 'input y:')
+        EditY1Cord = MyEdit(cords=Cords([160, 170, 100, 30]), textvariable=state.y1Value)
+
+        buttonAdd = Button(Cords([300, 100, 50, 30]), 'addButton', 'Add')
+        buttonAdd.onClick(addHandler)
+        myBtnField.addObject(buttonAdd)
+
+        myDC.setOnClick(clickHandler)
+
+        def key(event):
+            print("pressed", repr(event.char))
+
+        def callback(event):
+            myBtnField.clicked(event)
+
+        c.bind("<Key>", key)
+        c.bind("<Button-1>", callback)
+
+    App()
+
+    c.focus_set()
+    c.pack()
+
+
+forthLab()
 root.mainloop()
--- venv/myKinter.py
+++ venv/myKinter.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 from tkinter import *
 
 _root = None
@@ -8,7 +9,6 @@ class Root:
         self.root = root
         self.canvas = mainCanvas
         self.TIME_STEP = 50
-
 
     def setRoot(self):
         global _root
@@ -20,6 +20,11 @@ class Root:
 
     def getTimeStep(self):
         return self.TIME_STEP
+
+    def createDot(self, x, y, color='red', width=5):
+        print('[root.createDot] crods:', x, y)
+        return self.canvas.create_oval(x, y, x + width, y + width, width=0,
+                                         fill=color)
 
 # _root = Root(None, None)
 #
@@ -58,7 +63,7 @@ class Event:
 
 
 class Cords:
-    def __init__(self, cords, mode='rec'):
+    def __init__(self, cords, mode='rec'): #mode='x1y1'
         self.x = cords[0]
         self.y = cords[1]
         if mode == 'rec':
@@ -72,6 +77,7 @@ class Cords:
         self.x0y0x1y1 = [self.x, self.y, self.x1, self.y1]
         self.all = [self.x, self.y, self.width, self.height]
         self.x0y0 = [self.x, self.y]
+        self.x1y1 = [self.x1, self.y1]
 
 
 class MyCanvas:
@@ -89,8 +95,8 @@ class MyCanvas:
             return True
         return False
 
-    def clicked(self, event):
-        print(event)
+    # def clicked(self, event):
+    #     print(event)
 
     def setColor(self, color, time=1):
         setFill(_root.canvas, time, self.rectangle, color)
@@ -106,6 +112,33 @@ class ButtonField(MyCanvas):
             if self.checkCords(event, i.cords):
                 i.clicked(event)
 
+def xRotateMatrix(a):
+    return np.array([
+        [1, 0, 0],
+        [0, math.cos(math.radians(a)), -1 * math.sin(math.radians(a))],
+        [0, math.sin(math.radians(a)), math.cos(math.radians(a))]
+    ])
+
+def yRotateMatrix(a):
+    return np.array([
+        [math.cos(math.radians(a)), 0, math.sin(math.radians(a))],
+        [0, 1, 0],
+        [-1 * math.sin(math.radians(a)), 0, math.cos(math.radians(a))]
+    ])
+
+def zRotateMatrix(a):
+    return np.array([
+        [math.cos(math.radians(a)), -1 * math.sin(math.radians(a)), 0],
+        [math.sin(math.radians(a)), math.cos(math.radians(a)), 0],
+        [0, 0, 1]
+    ])
+
+def calculatePlotCords(cords):
+    a = 35
+    temp = cords.dot(yRotateMatrix(a))
+    temp = temp.dot(xRotateMatrix(-1 * a))
+    return [temp[0],temp[1]]
+
 GRID_STEP = 20
 
 class DrowingCanvas(ButtonField):
@@ -113,23 +146,69 @@ class DrowingCanvas(ButtonField):
     def __init__(self, cords):
         super().__init__(cords)
         self.onClickEvent = None
+        self.cords = cords
         self.rectangle = createRectangle(_root.canvas, cords, '#eeeeee')
-        # рисуем сетку
+        self.startDot = [cords.x, cords.y1]
+
+    def displayGrid(self):
+        cords = self.cords
+        endPoint = [cords.width//GRID_STEP, cords.height//GRID_STEP]
         for i in range(cords.width//GRID_STEP):
-            createLine(_root.canvas, Cords([cords.x + GRID_STEP * i, cords.y, 0, cords.height]),
+            createLine(_root.canvas, Cords([self.startDot[0] + GRID_STEP * i, self.startDot[1],
+                                            self.startDot[0] + GRID_STEP * i, self.startDot[1] - GRID_STEP * endPoint[1]],
+                                           'x1y1'),
                        fill='#bcbcbc')
-            _root.canvas.create_text(cords.x + GRID_STEP * i + 5, cords.y1 - GRID_STEP//2, text=i-1)
+            _root.canvas.create_text(self.startDot[0] + GRID_STEP * i - GRID_STEP//3, self.startDot[1] - GRID_STEP + GRID_STEP//2, text=i-1)
 
         for i in range(cords.height//GRID_STEP):
-            createLine(_root.canvas, Cords([cords.x, cords.y1 - GRID_STEP * i, cords.width,0]),
+            createLine(_root.canvas, Cords([self.startDot[0], self.startDot[1] - GRID_STEP * i,
+                                            self.startDot[0] + GRID_STEP * endPoint[0], self.startDot[1] - GRID_STEP * i],
+                                           'x1y1'),
                        fill='#bcbcbc')
-            _root.canvas.create_text(cords.x + GRID_STEP//2, cords.y1 - GRID_STEP * i + 5, text=i - 1)
+            _root.canvas.create_text(self.startDot[0] + 2 * GRID_STEP//3, self.startDot[1] - GRID_STEP * i - GRID_STEP + GRID_STEP//2, text=i - 1)
 
         self.Oy = createLine(_root.canvas,
-                             Cords([cords.x + GRID_STEP, cords.y + GRID_STEP, 0, cords.height - GRID_STEP]),
+                             Cords([cords.x + GRID_STEP, cords.y + GRID_STEP//2, 0, cords.height]),
                              fill='black', arrow=FIRST)
+        _root.canvas.create_text(cords.x + GRID_STEP + 5, cords.y + GRID_STEP - 5, text='Y')
+
         self.Ox = createLine(_root.canvas, Cords([cords.x, cords.y1 - GRID_STEP, cords.width - GRID_STEP, 0]),
                              fill='black', arrow=LAST)
+        _root.canvas.create_text(cords.x + cords.width - GRID_STEP + 5, cords.y1 - GRID_STEP - 5, text='X')
+
+
+    def getColor(self, a):
+        n = hex(a * 255 * 124).replace('0x', '#')
+        if len(n) < 7:
+            n = n[0] + '0' * (7 - len(n)) + n[1:]
+        return n
+
+    def create3DLine(self, start_point, end_point, color='black', arrow=None):
+        plotCordsB = calculatePlotCords(start_point)
+        plotCordsE = calculatePlotCords(end_point)
+        return  createLine(_root.canvas,
+                   Cords([self.startDot[0] + plotCordsB[0] * GRID_STEP, self.startDot[1] - plotCordsB[1] * GRID_STEP,
+                          self.startDot[0] + plotCordsE[0] * GRID_STEP, self.startDot[1] - plotCordsE[1] * GRID_STEP],
+                         mode='x1y1'),
+                   fill=color, arrow=arrow)
+
+    def displayGrid3D(self):
+        cords = self.cords
+        endpoint = 15
+        XYZ = np.array([[endpoint, 0, 0], [0, endpoint, 0], [0, 0, endpoint]])
+        self.startDot=[cords.x + GRID_STEP * 12, cords.y1 - GRID_STEP * 13]
+        sample = [[1,0,0],[0,1,0],[0,0,1]]
+        for k in sample:
+            for j in sample:
+                for i in range(1, endpoint):
+                    if j!=k:
+                        tempB = np.array([i * j[0], i * j[1], i * j[2]])
+                        tempE = np.array([i * j[0] + k[0] * endpoint, i * j[1] + k[1] * endpoint, i * j[2] + k[2] * endpoint])
+                        self.create3DLine(tempB, tempE, color='#bcbcbc')
+        for i in XYZ:
+            plotCords = calculatePlotCords(i)
+            self.create3DLine(np.array([0, 0, 0]), i, arrow=LAST)
+            _root.canvas.create_text(self.startDot[0] + plotCords[0] * GRID_STEP, self.startDot[1] - plotCords[1] * GRID_STEP, text=i)
 
     def setOnClick(self, event):
         self.onClickEvent = event
@@ -146,8 +225,8 @@ class DrowingCanvas(ButtonField):
         return None
 
     def calculateCordsByXY(self, x, y):
-        resX = (x + 1) * GRID_STEP + self.cords.x
-        resY = self.cords.y1 - (y + 1) * GRID_STEP
+        resX = (x + 1) * GRID_STEP  + self.startDot[0]
+        resY = self.startDot[1] - (y + 1) * GRID_STEP
         return [resX,resY]
 
     def calculateNearCords(self, x, y):
@@ -159,6 +238,17 @@ class DrowingCanvas(ButtonField):
         resX = (x - self.cords.x) / GRID_STEP - 1
         resY = (self.cords.y + self.cords.height - y) / GRID_STEP - 1
         return [resX,resY]
+
+    def createDot(self, x, y, color='red', width=5):
+        return _root.createDot(x, y, color, width)
+
+    def createPoint(self, x, y, color='red', width=5):
+        cords = self.calculateCordsByXY(x, y)
+        return self.createDot(cords[0], cords[1], color, width)
+
+    def create3DPoint(self, cords, color='red', width=5):
+        res_cords = calculatePlotCords(cords)
+        return self.createPoint(res_cords[0], res_cords[1], color, width)
 
 class Button:
 
@@ -259,6 +349,69 @@ class Lable:
 
     # def __del__(self):
     #     deleteObjFromCanvas(_root.canvas, self.text)
+
+class Rectangle:
+
+    def __init__(self, cords, color='black'): #cords = [x,y,z]
+        self.color = color
+        self.cords = cords
+        self.lines = []
+        n = 4
+        points = [[cords[0], cords[1]], [cords[0], cords[3]], [cords[2], cords[3]], [cords[2], cords[1]]]
+        for i in range(0, n, 2):
+            self.lines.append(
+                Line(Cords([points[i][0], points[i][1], points[(n + i + 1) % n][0], points[(n + i + 1) % n][1]],
+                           'x1y1'), str(i)+'-'+str((n + i + 1) % n), color))
+            self.lines.append(
+                Line(Cords([points[i][0], points[i][1], points[(n + i - 1) % n][0], points[(n + i - 1) % n][1]],
+                           'x1y1'), str(i)+'-'+str((n + i - 1) % n), color))
+
+
+    def checkCords(self, cords): # cords = [x,y]
+        vis = 0
+        if cords[0] < self.cords[0]:
+            vis += 8
+        if cords[0] > self.cords[2]:
+            vis += 4
+        if cords[1] > self.cords[1]:
+            vis += 2
+        if cords[1] < self.cords[3]:
+            vis += 1
+        return vis
+
+class Dot:
+
+    def __init__(self, cords, color='black', width=5, mode='dot', pointCords=None): #cords = [x,y,z]
+        print('[Dot/init] pointCords:', pointCords)
+        self.pointCords = pointCords
+        self.width = width
+        self.color = color
+        self.cords = cords
+        self.point = _root.createDot(cords[0], cords[1], color, width)
+        self.visability = True
+
+    def setVisability(self, value):
+        self.visability = False
+        if self.visability:
+            deleteObjFromCanvas(_root.canvas, self.point)
+        else:
+            self.point = _root.createDot(cords[0], cords[1], color, width)
+
+    def onClick(self, event):
+        self.onClickEvent = event
+
+    def clicked(self, e):
+        event = Event(e)
+        event.btnPressed = self
+        if self.onClickEvent:
+            self.onClickEvent(event)
+
+    def moveIt(self, cords, time=1):
+        self.cords = cords
+        moveFigeure(_root.canvas, time, self.text, self.cords.x0y0x1y1)
+
+    def __del__(self):
+        deleteObjFromCanvas(_root.canvas, self.text)
 
 
 class MyEdit:
